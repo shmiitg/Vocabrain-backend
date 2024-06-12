2fb697c71ab425cce6a240c735dfabd7a853a6a2
Shubham Mondal <shubhamank002@gmail.com>
2024-06-12 17:16:10 +0530

    update db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,14 @@ const DB = process.env.USER_DB;
 // const DB = process.env.LOCAL_USER_DB;
 
 mongoose
-    .connect(DB, { useNewUrlParser: true, wtimeoutMS: 2500 })
-    .then(() => console.log("Database connected..."))
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 10000,
+        wtimeoutMS: 10000,
+    })
+    .then(() => {
+        console.log("Database connected...");
+        mongoose.connection.close();
+    })
     .catch((err) => console.log("Error: ", err));
--- server.js
+++ server.js
@@ -14,4 +14,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server running at port ${port}`));
